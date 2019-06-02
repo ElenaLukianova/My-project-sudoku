595c0f8a9825eb1bd7a8f0bce19141129c2ccde1
ElenaLukianova <51290737+ElenaLukianova@users.noreply.github.com>
2019-06-02 23:41:40 +0600

    Sudoku Tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sudoku;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Sudoku.Game c = new Game();
+            Random r = new Random();
+            c.NewGame(r);
+            Assert.AreEqual(c.r, r);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Sudoku.Game f = new Game();
+            Random r = new Random();
+            f.NewGame(r);
+            Assert.AreEqual(f.r, r);
+        }
+    }
+}
